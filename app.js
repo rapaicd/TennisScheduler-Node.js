e9a57d61bb14f2e30d76dd5495a31643ba3950e8
Danilo Rapaic <93617683+rapaicd@users.noreply.github.com>
2023-03-15 09:13:30 +0100

    feat: added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const timeslotRoutes = require('./routes/timeslotRoutes')
 const userRoutes = require('./routes/userRoutes')
 const tennisCourtRoutes = require('./routes/tennisCourtRoutes')
@@ -6,6 +7,7 @@ const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 
 const app = express();
+app.use(cors())
 
 app.use(express.json())
 app.use('/users',userRoutes)
